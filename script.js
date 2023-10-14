aec04f0ae12a3d6697e1c1a6a647758277c088e5
DevQueen <124140753+oyingrace@users.noreply.github.com>
2023-10-14 19:07:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,9 @@
-let [seconds, minutes, hours] =[0,0,0];
-let displayTime = document.getElementById("displayTime");
-let timer = null;
 
-function stopwatch(){
-    seconds++;
-    if (seconds == 60){
-        seconds = 0;
-        minutes++;
-        if (minutes == 60){
-            minutes = 0;
-            hours++;
-        }
-    }
-    let h = hours <10 ? "0" + hours : hours;
-    let m = minutes <10 ? "0" + minutes : minutes;
-    let s = seconds <10 ? "0" + seconds : seconds;
-    
-    displayTime.innerHTML = h +" : "+ m +": "+s;
-}
-function watchStart(){
-    if(timer != null){
-        clearInterval(timer);
-    }
-   timer = setInterval(stopwatch,100);
+let heart = document.getElementById("heart");
 
+function openHeart(){
+   heart.classList.add("open-heart");
 }
-function watchPause(){
-    clearInterval(timer);
-}
-function watchReset(){
-    clearInterval(timer);
-    [seconds, minutes, hours] =[0,0,0];
-    displayTime.innerHTML = "00:00:00";
-}
+function closeHeart(){
+    heart.classList.remove("open-heart");
+ }
